12dc0e03902934f88936c0362ecabf5d2ab2e1c1
Tudwal <tudwal.corlouer2021@campus-eni.fr>
2022-03-14 14:52:53 +0100

    il faut commit

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -216,7 +216,7 @@ class EventController extends AbstractController
      */
     public function cancel(StateRepository $stateRepo ,Event $e,EntityManagerInterface $em , EventRepository $repo, $id, Request $req): Response
     {
-        $events = $repo->find($id);
+       // $events = $repo->find($id);
                        
         if($req->get('motif_cancel'))
         {
@@ -229,7 +229,6 @@ class EventController extends AbstractController
         }        
         return $this->render('event/cancel.html.twig',[
             'event'=>$e,
-            'events'=>$events,
         ]);
     }
 
