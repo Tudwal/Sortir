9045c7760bb70cd013a930d10d70fd2be3308d86
Claire GOARNISSON <claire.goarnisson2021@campus-eni.fr>
2022-03-15 10:10:48 +0100

    beautifull unicorn

--- src/Form/EventTypeAPI.php
+++ src/Form/EventTypeAPI.php
@@ -80,7 +80,7 @@ class EventTypeAPI extends AbstractType
                     'mapped' => false,
                     'class' => City::class,
                     'choice_label' => 'name',
-                     'placeholder' => "Choisir une ville"
+                    'placeholder' => "Choisir une ville"
                 ]
             );
     }
